0736a90b298d998fd9cc80696802185bd02df950
Eypz God <168025354+eypz@users.noreply.github.com>
2024-06-03 00:31:54 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://godzenitsu:godzenitsu@cluster0.czhj8cm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
-  SESSION_NAME: process.env.SESSION_NAME || "QUEEN-NEZUKO~"
+  MONGODB_URL: process.env.MONGODB_URL || "mongodb+srv://eypzbuddy:cmoflChJCdpd94EE@izumi-eypz.vwpdjxv.mongodb.net/?retryWrites=true&w=majority&appName=izumi-eypz",
+  SESSION_NAME: process.env.SESSION_NAME || "IZUMI-V2~"
 };
